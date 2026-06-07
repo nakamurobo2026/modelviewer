e67413ef880d29eb8e9848aef0b96e19723a824a
nakamurobo2026 <nakamura0407@gmail.com>
2026-06-07 21:20:29 +0900

    Bump iwakan lab cache version

--- iwakan-lab/service-worker.js
+++ iwakan-lab/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "iwakan-lab-v3";
+const CACHE_NAME = "iwakan-lab-v4";
 const ASSETS = [
   "./",
   "./index.html",
@@ -11,18 +11,12 @@ const ASSETS = [
 ];
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
-    )
-  );
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))));
   self.clients.claim();
 });
 
